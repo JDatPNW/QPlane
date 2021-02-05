3fc2900c87f6de3d5dde2a05d1d6ff7acde4193c
JD <david.richter@freenet.de>
2021-02-04 21:36:50 -0600

    Added plotting file

---
+++ utils/plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+results = np.load("results.npy", allow_pickle=True)
+
+plt.plot(results.item().get('epoch'), results.item().get('average'), label="average rewards")
+plt.plot(results.item().get('epoch'), results.item().get('maximum'), label="max rewards")
+plt.plot(results.item().get('epoch'), results.item().get('minimum'), label="min rewards")
+plt.legend(loc=4)
+plt.show()

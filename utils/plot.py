d7f958219d3b9a0d5615f0c4edbd90a6ee6108c1
JD <david.richter@freenet.de>
2021-03-20 21:55:07 -0500

    added Q metric to utils/plot

--- utils/plot.py
+++ utils/plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 results = np.load("results.npy", allow_pickle=True)
 
 plt.plot(results.item().get('epoch'), results.item().get('average'), label="average rewards")
+plt.plot(results.item().get('epoch'), results.item().get('averageQ'), label="average Qs")
 plt.plot(results.item().get('epoch'), results.item().get('maximum'), label="max rewards")
 plt.plot(results.item().get('epoch'), results.item().get('minimum'), label="min rewards")
 plt.legend(loc=4)

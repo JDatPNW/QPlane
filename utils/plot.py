2584753991c4dfa3893892d493ac7094a4231ebe
JD <david.richter@freenet.de>
2021-05-21 20:24:36 -0500

    added if statements for what to print

--- utils/plot.py
+++ utils/plot.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plotAverage = True
+plotAverageQ = True
+plotMaximum = True
+plotMinimum = True
+plotEpsilon = True
+
 results = np.load("results.npy", allow_pickle=True)
 
 average = 2. * (np.array(results.item().get('average')) - np.min(np.array(results.item().get('average')))) / np.ptp(np.array(results.item().get('average'))) - 1
@@ -9,20 +15,20 @@ maximum = 2. * (np.array(results.item().get('maximum')) - np.min(np.array(result
 minimum = 2. * (np.array(results.item().get('minimum')) - np.min(np.array(results.item().get('minimum')))) / np.ptp(np.array(results.item().get('minimum'))) - 1
 epsilon = 2. * (np.array(results.item().get('epsilon')) - np.min(np.array(results.item().get('epsilon')))) / np.ptp(np.array(results.item().get('epsilon'))) - 1
 
-plt.plot(results.item().get('epoch'), average, label="average rewards")
-plt.plot(results.item().get('epoch'), averageQ, label="average Qs")
-plt.plot(results.item().get('epoch'), maximum, label="max rewards")
-plt.plot(results.item().get('epoch'), minimum, label="min rewards")
-plt.plot(results.item().get('epoch'), epsilon, label="epsilon")
+if(plotAverage): plt.plot(results.item().get('epoch'), average, label="average rewards")
+if(plotAverageQ): plt.plot(results.item().get('epoch'), averageQ, label="average Qs")
+if(plotMaximum): plt.plot(results.item().get('epoch'), maximum, label="max rewards")
+if(plotMinimum): plt.plot(results.item().get('epoch'), minimum, label="min rewards")
+if(plotEpsilon): plt.plot(results.item().get('epoch'), epsilon, label="epsilon")
 plt.title("Normalized Results")
 plt.legend(loc=4)
 plt.show()
 
-plt.plot(results.item().get('epoch'), results.item().get('average'), label="average rewards")
-plt.plot(results.item().get('epoch'), results.item().get('averageQ'), label="average Qs")
-plt.plot(results.item().get('epoch'), results.item().get('maximum'), label="max rewards")
-plt.plot(results.item().get('epoch'), results.item().get('minimum'), label="min rewards")
-plt.plot(results.item().get('epoch'), results.item().get('epsilon'), label="epsilon")
+if(plotAverage): plt.plot(results.item().get('epoch'), results.item().get('average'), label="average rewards")
+if(plotAverageQ): plt.plot(results.item().get('epoch'), results.item().get('averageQ'), label="average Qs")
+if(plotMaximum): plt.plot(results.item().get('epoch'), results.item().get('maximum'), label="max rewards")
+if(plotMinimum): plt.plot(results.item().get('epoch'), results.item().get('minimum'), label="min rewards")
+if(plotEpsilon): plt.plot(results.item().get('epoch'), results.item().get('epsilon'), label="epsilon")
 plt.title("Results")
 plt.legend(loc=4)
 plt.show()

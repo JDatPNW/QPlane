c89fab588e6697d43d07608db2c495cac951db52
JD <david.richter@freenet.de>
2021-06-18 00:02:11 -0500

    modded JSBSim Env for scenarios

--- src/environments/jsbsim/JSBSimEnv.py
+++ src/environments/jsbsim/JSBSimEnv.py
@@ -6,7 +6,8 @@ import time
 
 class Env():
 
-    def __init__(self, orig, dest, n_acts, usePredefinedSeeds, dictObservation, dictAction, dictRotation, speed, pause, qID, render, realTime):
+    def __init__(self, scene, orig, dest, n_acts, usePredefinedSeeds, dictObservation, dictAction, dictRotation, speed, pause, qID, render, realTime):
+        self.scenario = scene
         self.startingPosition = orig
         self.destinationPosition = dest
         self.previousPosition = orig
@@ -78,16 +79,6 @@ class Env():
 
         return values
 
-    def getTermination(self):
-
-        if (self.fdm["position/h-agl-ft"] < 200):  # checks if plane is less than x feet off the ground, if not it will count as a crash
-            terminate = True
-        elif(self.fdm["aero/alpha-deg"] >= 16):
-            terminate = True
-        else:
-            terminate = False
-        return terminate
-
     def send_Ctrl(self, ctrl):
         '''
         ctrl[0]: + Stick in (elevator pointing down) / - Stick back (elevator pointing up)
@@ -113,133 +104,24 @@ class Env():
         return r
 
     def getControl(self, action, observation):
-        # translate the action to the controll space value
-        actionCtrl = 0
-        if(action <= self.dictAction["pi-"]):
-            actionCtrl = 0
-        elif(action <= self.dictAction["ro-"]):
-            actionCtrl = 1
-        elif action <= self.dictAction["ru-"]:
-            actionCtrl = 2
-
-        ctrl = [0, 0, 0, 0.5, -998, -998]  # throttle set to .5 by default
-        # translate the action value to the observation space value
-        actionDimension = 3 + actionCtrl
-
-        # this is to make actions less significant if the plane is more stable
-        if(action != self.dictAction["no"]):
-            if observation[actionDimension] < -180 or observation[actionDimension] > 180:
-                ctrl[actionCtrl] = 1
-            elif -180 <= observation[actionDimension] < -50 or 50 <= observation[actionDimension] < 180:
-                ctrl[actionCtrl] = 0.5
-            elif -50 <= observation[actionDimension] < -25 or 25 <= observation[actionDimension] < 50:
-                ctrl[actionCtrl] = 0.2
-            elif -25 <= observation[actionDimension] < -15 or 15 <= observation[actionDimension] < 25:
-                ctrl[actionCtrl] = 0.15
-            elif -15 <= observation[actionDimension] < -10 or 10 <= observation[actionDimension] < 15:
-                ctrl[actionCtrl] = 0.12
-            elif -10 <= observation[actionDimension] < -5 or 5 <= observation[actionDimension] < 10:
-                ctrl[actionCtrl] = 0.1
-            elif -5 <= observation[actionDimension] < -2 or 2 <= observation[actionDimension] < 5:
-                ctrl[actionCtrl] = 0.05
-            elif -2 <= observation[actionDimension] < -1 or 1 <= observation[actionDimension] < 2:
-                ctrl[actionCtrl] = 0.02
-            elif -1 <= observation[actionDimension] < 0 or 0 <= observation[actionDimension] < 1:
-                ctrl[actionCtrl] = 0.01
-            else:
-                print("DEBUG - should not get here")
-        else:
-            ctrl = [0, 0, 0, 0.5, -998, -998]
-
-        if (actionCtrl == 2):
-            ctrl[actionCtrl] = 0.01  # Doing this because the pedals don't work with the applied degree idea
-
-        if(action % 2 != 0):  # check if action should be positive or negative
-            ctrl[actionCtrl] = -ctrl[actionCtrl]
-
-        actions_binary = np.zeros(self.n_actions, dtype=int)
-        actions_binary[action] = 1
+        ctrl, actions_binary = self.scenario.getControl(action, observation)
 
         return ctrl, actions_binary
-
-    def encodeState(self, pitch, roll, yaw):
-        i = pitch
-        i = i * 9
-        i = i + roll
-        i = i * 9
-        i = i + yaw
-        return i
-
-    def encodeRotation(self, i):
-        if -180 <= i < -35:
-            return 0
-        elif -35 <= i < -25:
-            return 1
-        elif -25 <= i < -15:
-            return 2
-        elif -15 <= i < -5:
-            return 3
-        elif -5 <= i < 5:
-            return 4
-        elif 5 <= i < 15:
-            return 5
-        elif 15 <= i < 25:
-            return 6
-        elif 25 <= i < 35:
-            return 7
-        elif 35 <= i < 180:
-            return 8
-        else:
-            return 0
-
-    def encodeRotations(self, pitch, roll, yaw):
-        pitchEnc = self.encodeRotation(pitch)
-        rollEnc = self.encodeRotation(roll)
-        yawEnc = self.encodeRotation(yaw)
-        return pitchEnc, rollEnc, yawEnc
 
     def getDeepState(self, observation):
         velocities = self.getVelo()
         positions = observation[3:]
-        vel = []
-        for i in range(len(velocities)):
-            vel.append(velocities[i])
-        state = tuple(positions) + tuple(vel)
+        state = self.scenario.getDeepState(velocities, positions)
         return state
 
     def getState(self, observation):
-        pitch = observation[self.dictObservation["pitch"]]
-        roll = observation[self.dictObservation["roll"]]
-        yaw = observation[self.dictObservation["yaw"]]
-        pitchEnc, rollEnc, yawEnc = self.encodeRotations(pitch, roll, yaw)
-
-        state = self.encodeState(pitchEnc, rollEnc, yawEnc)
-        return int(state)
+        state = self.scenario.getState(observation)
+        return state
 
     def rewardFunction(self, action, newObservation):
-        roll = float(abs(newObservation[self.dictObservation["roll"]] / 180))
-        pitch = float(abs(newObservation[self.dictObservation["pitch"]] / 180))
-        reward = pow(float((2 - (roll + pitch)) / 2), 2)
-
-        if(abs(newObservation[self.dictObservation["roll"]]) > 40 or abs(newObservation[self.dictObservation["pitch"]]) > 40):
-            reward = reward * 0.1
-        elif(abs(newObservation[self.dictObservation["roll"]]) > 20 or abs(newObservation[self.dictObservation["pitch"]]) > 20):
-            reward = reward * 0.25
-        elif(abs(newObservation[self.dictObservation["roll"]]) > 10 or abs(newObservation[self.dictObservation["pitch"]]) > 10):
-            reward = reward * 0.5
-        elif(abs(newObservation[self.dictObservation["roll"]]) > 5 or abs(newObservation[self.dictObservation["pitch"]]) > 5):
-            reward = reward * 0.75
-        elif(abs(newObservation[self.dictObservation["roll"]]) > 1 or abs(newObservation[self.dictObservation["pitch"]]) > 1):
-            reward = reward * 0.9
-
-        if(newObservation[self.dictObservation["alt"]] <= 1000):
-            reward = reward * 0.1
-
-        done = False
-        if(self.getTermination()):  # Would be used for end parameter - for example, if plane crahsed done, or if plane reached end done
-            done = True
-            reward = -1
-
+        alt = self.fdm["position/h-agl-ft"]
+        alpha = self.fdm["aero/alpha-deg"]
+        reward, done = self.scenario.rewardFunction(action, newObservation, alt, alpha)
         return reward, done
 
     def step(self, action):

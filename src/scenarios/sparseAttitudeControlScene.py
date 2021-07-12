d9e0fa9dfec8970bce916d8c4080fe8f664f4d15
JD <david.richter@freenet.de>
2021-07-12 16:07:44 -0500

    added deep state to sparse scenario

--- src/scenarios/sparseAttitudeControlScene.py
+++ src/scenarios/sparseAttitudeControlScene.py
@@ -3,10 +3,12 @@ import numpy as np
 
 class Scene():
 
-    def __init__(self, dictObservation, dictAction, actions):
+    def __init__(self, dictObservation, dictAction, actions, stateDepth):
         self.dictObservation = dictObservation
         self.dictAction = dictAction
         self.n_actions = actions
+        self.stateList = []
+        self.stateDepth = stateDepth
         self.id = "sparseAttitude"
 
     def getTermination(self, alt, alpha):
@@ -91,7 +93,10 @@ class Scene():
         for i in range(len(velocities)):
             vel.append(velocities[i])
         state = tuple(positions) + tuple(vel)
-        return state
+        self.stateList.append(state)
+        if(len(self.stateList) > self.stateDepth):
+            self.stateList.pop(0)
+        return self.stateList
 
     def getState(self, observation):
         pitch = observation[self.dictObservation["pitch"]]
@@ -137,3 +142,6 @@ class Scene():
         rollEnc = self.encodeRotation(roll)
         yawEnc = self.encodeRotation(yaw)
         return pitchEnc, rollEnc, yawEnc
+
+    def resetStateDepth(self):
+        self.stateList = []

5bc3836b6ff967161cfcbb5473d4b5e61022be9c
JD <david.richter@freenet.de>
2021-07-12 17:42:21 -0500

    added option to show all or only not NaN rows in tableReader

--- utils/tableReader.py
+++ utils/tableReader.py
@@ -2,9 +2,11 @@ import numpy as np
 import pandas as pd
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
+onlyShowTrained = True  # only shows rows that are non NAN
+
 table = np.load("model.npy")
 table = pd.DataFrame(table)
-table = table[(table.T != 0).any()]
-
+if(onlyShowTrained):
+    table = table[(table.T != 0).any()]
 
 print(table)

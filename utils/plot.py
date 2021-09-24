a621588cc0598e87d6e3f14704972871bc3fa3ce
JD <JDatPNW@users.noreply.github.com>
2021-09-24 11:39:42 -0500

    changed plot ormalization to range 0 to 1 from -1 to 1

--- utils/plot.py
+++ utils/plot.py
@@ -13,20 +13,20 @@ results = np.load("results.npy", allow_pickle=True)
 
 if(plotNormalized):
     if(plotAverage):
-        average = 2. * (np.array(results.item().get('average')) - np.min(np.array(
-            results.item().get('average')))) / np.ptp(np.array(results.item().get('average'))) - 1
+        average = (np.array(results.item().get('average')) - np.min(np.array(
+            results.item().get('average')))) / np.ptp(np.array(results.item().get('average')))
     if(plotAverageQ):
-        averageQ = 2. * (np.array(results.item().get('averageQ')) - np.min(np.array(
-            results.item().get('averageQ')))) / np.ptp(np.array(results.item().get('averageQ'))) - 1
+        averageQ = (np.array(results.item().get('averageQ')) - np.min(np.array(
+            results.item().get('averageQ')))) / np.ptp(np.array(results.item().get('averageQ')))
     if(plotMaximum):
-        maximum = 2. * (np.array(results.item().get('maximum')) - np.min(np.array(
-            results.item().get('maximum')))) / np.ptp(np.array(results.item().get('maximum'))) - 1
+        maximum = (np.array(results.item().get('maximum')) - np.min(np.array(
+            results.item().get('maximum')))) / np.ptp(np.array(results.item().get('maximum')))
     if(plotMinimum):
-        minimum = 2. * (np.array(results.item().get('minimum')) - np.min(np.array(
-            results.item().get('minimum')))) / np.ptp(np.array(results.item().get('minimum'))) - 1
+        minimum = (np.array(results.item().get('minimum')) - np.min(np.array(
+            results.item().get('minimum')))) / np.ptp(np.array(results.item().get('minimum')))
     if(plotEpsilon):
-        epsilon = 2. * (np.array(results.item().get('epsilon')) - np.min(np.array(
-            results.item().get('epsilon')))) / np.ptp(np.array(results.item().get('epsilon'))) - 1
+        epsilon = (np.array(results.item().get('epsilon')) - np.min(np.array(
+            results.item().get('epsilon')))) / np.ptp(np.array(results.item().get('epsilon')))
 
     if(plotAverage):
         plt.plot(results.item().get('epoch'), average, label="average rewards")
